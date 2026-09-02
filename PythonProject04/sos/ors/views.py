c4dcb5dca02c3f4a7a860c33d232e26d693d2211
Harshad-Niboriya <niboriyaharshad@gmail.com>
2026-09-02 16:35:02 +0530

    02 SEPTEMBER

---
+++ PythonProject04/sos/ors/views.py
@@ -0,0 +1,26 @@
+from django.http import HttpResponse
+
+from django.shortcuts import render
+
+def test_ors(request):
+    return HttpResponse('<h1> ors test view function</h1>')
+
+def display(request):
+    return HttpResponse('<h1>ors display function</h1>')
+
+def welcome(request):
+    return render(request, 'welcome.html')
+
+def user_signup(request):
+    print(request.POST.get('firstName'))
+    print(request.POST.get('lastName'))
+    print(request.POST.get('loginID'))
+    print(request.POST.get('password'))
+    print(request.POST.get('DOB'))
+    print(request.POST.get('Address'))
+    return render(request, 'registration.html')
+
+def user_signin(request):
+    print(request.POST.get('firstName'))
+    print(request.POST.get('lastName'))
+    return render(request, 'login.html')
