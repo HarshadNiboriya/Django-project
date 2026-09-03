2aba63eade519ef0a0a66f70f45f3e825854d41e
Harshad-Niboriya <niboriyaharshad@gmail.com>
2026-09-03 13:26:07 +0530

    03 SEPT

--- PythonProject04/sos/ors/views.py
+++ PythonProject04/sos/ors/views.py
@@ -12,6 +12,12 @@ def welcome(request):
     return render(request, 'welcome.html')
 
 def user_signup(request):
+    # print(request.GET.get('firstName'))
+    # print(request.GET.get('lastName'))
+    # print(request.GET.get('loginID'))
+    # print(request.GET.get('Password'))
+    # print(request.GET.get('Address'))
+    # print(request.GET.get('DOB'))
     print(request.POST.get('firstName'))
     print(request.POST.get('lastName'))
     print(request.POST.get('loginID'))
